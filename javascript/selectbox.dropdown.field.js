53d2cad1d6ddd39da971b214b4d41a361286ab13
muskie9 <nhorstmeier@gmail.com>
2016-02-29 22:31:17 -0600

    UPDATE select init formatting

--- javascript/selectbox.dropdown.field.js
+++ javascript/selectbox.dropdown.field.js
@@ -1,13 +1,13 @@
-(function($) {
+(function ($) {
 
     $.fn.extend({
         ssSelectbox: function (opts) {
             return $(this).each(function () {
-             var config = $.extend(opts || {}, $(this).data(), $(this).data('selectboxconfig'), {});
-             if ($(this).hasClass('selectbox-applied')) return; // already applied
-             $(this).addClass('selectbox-applied').selectbox(config);
-             $(this).blur().focus(); // trigger show
-             });
+                var config = $.extend(opts || {}, $(this).data(), $(this).data('selectboxconfig'), {});
+                if ($(this).hasClass('selectbox-applied')) return; // already applied
+                $(this).addClass('selectbox-applied').selectbox(config);
+                $(this).blur().focus(); // trigger show
+            });
         }
     });
 
